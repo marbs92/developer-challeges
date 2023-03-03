966d873756db8203d457b1665b14a42efb090bbb
Rocha <mrocha.dev92@gmail.com>
2023-03-03 16:23:36 -0600

    feat: login view

---
+++ TestCoopelRepos/app/src/main/java/com/example/testcoopelrepos/login/LoginFragment.kt
@@ -0,0 +1,37 @@
+package com.example.testcoopelrepos.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.testcoopelrepos.R
+import com.example.testcoopelrepos.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+
+    private lateinit var binding: FragmentLoginBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentLoginBinding.bind(view)
+        setupView()
+        initObservers()
+    }
+
+    private fun initObservers() {
+
+    }
+
+    private fun setupView() {
+        binding.apply {
+            btnLogin.setOnClickListener {
+
+            }
+        }
+    }
+
+
+}
