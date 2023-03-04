c5d30e1693553ca984d87b0c4734bcdc72cae9b8
Rocha <mrocha.dev92@gmail.com>
2023-03-03 22:35:44 -0600

    feat: firebaseUtils and sealed class Resource

---
+++ TestCoopelRepos/data/src/main/java/com/example/data/generic/Resource.kt
@@ -0,0 +1,7 @@
+package com.example.data.generic
+
+sealed class Resource<out R> {
+    data class Success<out R>(val result: R) : Resource<R>()
+    data class Failure(val message: String) : Resource<Nothing>()
+    object Loading : Resource<Nothing>()
+}
