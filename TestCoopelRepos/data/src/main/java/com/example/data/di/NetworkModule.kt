6dcab8f6d5815edc7170855bec51b7cbece70c3b
Rocha <mrocha.dev92@gmail.com>
2023-03-05 00:43:55 -0600

    feat: NetworkModule (di)

---
+++ TestCoopelRepos/data/src/main/java/com/example/data/di/NetworkModule.kt
@@ -0,0 +1,57 @@
+package com.example.data.di
+
+
+import com.example.data.BuildConfig
+import com.example.data.service.GenericServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient{
+        val httpLoginInterceptor = HttpLoggingInterceptor()
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoginInterceptor.apply {
+                httpLoginInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            })
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
+
+
+
+    @Singleton
+    @Provides
+    fun provideRetrofitClient(
+        okHttpClient: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BuildConfig.BASE_URL)
+            .build()
+
+
+
+    @Singleton
+    @Provides
+    fun providesGenericServicesImplementation(
+        retrofit: Retrofit): GenericServices =
+        retrofit.create(GenericServices::class.java)
+
+}
