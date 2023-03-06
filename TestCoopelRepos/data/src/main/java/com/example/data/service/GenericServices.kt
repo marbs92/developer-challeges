6859bcda7d8c9e79ecaa9a9eac87b14de6d0a730
Rocha <mrocha.dev92@gmail.com>
2023-03-06 00:35:03 -0600

    feat: GenericServices

---
+++ TestCoopelRepos/data/src/main/java/com/example/data/service/GenericServices.kt
@@ -0,0 +1,36 @@
+package com.example.data.service
+
+import com.example.domain.network.response.readme.ReadmeResponseModel
+import com.example.domain.network.response.repos.GetReposResponseModelItem
+import com.example.domain.network.response.tags.GetTagsResponseModelItem
+import retrofit2.Call
+import retrofit2.http.*
+
+interface GenericServices {
+
+
+    @GET("/repos/{owner}/{repo}/tags")
+    fun getTagsList(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String,
+        @Header("Authorization") authorization: String,
+    ): Call<List<GetTagsResponseModelItem>>
+
+    @GET("/repos/{owner}/{repo}/readme")
+    fun getReadme(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String,
+        @Header("Authorization") authorization: String,
+      //  @Header("Accept") accept: String,
+    ): Call<ReadmeResponseModel>
+
+
+    @GET("users/{username}/repos")
+    fun getRepos(
+        @Path("username") username: String,
+        @Header("Authorization") authorization: String,
+        @Query("page") page: Int,
+        @Query("per_page") pageSize: Int
+    ): Call<List<GetReposResponseModelItem>>
+
+}
