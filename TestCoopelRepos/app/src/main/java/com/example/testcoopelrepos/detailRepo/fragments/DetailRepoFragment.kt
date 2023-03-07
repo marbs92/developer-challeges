6b0f05c05d8a86e8b3ac2bec729b3bd0d68ec458
Rocha <mrocha.dev92@gmail.com>
2023-03-07 04:18:55 -0600

    feat: calling getTags and gerReadme

--- TestCoopelRepos/app/src/main/java/com/example/testcoopelrepos/detailRepo/fragments/DetailRepoFragment.kt
+++ TestCoopelRepos/app/src/main/java/com/example/testcoopelrepos/detailRepo/fragments/DetailRepoFragment.kt
@@ -1,33 +1,106 @@
 package com.example.testcoopelrepos.detailRepo.fragments
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.View
+import android.graphics.Color
+import android.util.Base64
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
+import com.example.domain.network.response.tags.GetTagsResponseModelItem
 import com.example.testcoopelrepos.R
+import com.example.testcoopelrepos.basecomponents.BaseFragment
+import com.example.testcoopelrepos.basecomponents.ShowDialog.showDialogInfo
 import com.example.testcoopelrepos.databinding.FragmentDetailRepoBinding
+import com.example.testcoopelrepos.detailRepo.viewModels.DetailRepoFragmentViewModel
+import com.example.testcoopelrepos.main.viewModels.MainActivityViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class DetailRepoFragment : Fragment(R.layout.fragment_detail_repo) {
+class DetailRepoFragment :
+    BaseFragment<FragmentDetailRepoBinding, DetailRepoFragmentViewModel>(DetailRepoFragmentViewModel::class) {
 
 
-    lateinit var binding: FragmentDetailRepoBinding
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding = FragmentDetailRepoBinding.bind(view)
-        initObservers()
-        setupView()
+    private val args: DetailRepoFragmentArgs by navArgs()
+    override fun getViewBinding() = FragmentDetailRepoBinding.inflate(layoutInflater)
 
+    override fun initObservers() {
+        viewModel.tagsResult.observe(viewLifecycleOwner) { result ->
+            result?.apply {
+                if (result.status == 200) {
+                    setupTags(result.data!!)
+                } else {
+                    showDialogInfo(requireActivity(), message = result.message, btnAceptar = {
 
+                    })
+                }
+            }
+        }
+        viewModel.readmeResult.observe(viewLifecycleOwner) { result ->
+            result?.apply {
+                hideProgressBarCustom()
+                if (result.status == 200) {
+                    setReadmeInTextView(result.data?.content)
+                } else {
+                    showDialogInfo(requireActivity(), message = result.message)
+                }
+            }
+        }
     }
 
-    private fun initObservers() {
+    override fun setupView() {
+        showProgressBarCustom()
 
+        binding.apply {
+            tvRepoName.text = args.repoName
+            tvBranch.text = args.branchName
+            tvSize.text = requireActivity().getString(R.string.size, args.size)
+
+        }
+
+        viewModel.apply {
+            lifecycleScope.launch {
+                getUserData()
+                getTagsList(userData.value!!.userName, args.repoName)
+                getReadmeList(userData.value!!.userName, args.repoName)
+            }
+        }
     }
 
-    private fun setupView() {
+    private fun setTagIntoView(tag: String) {
+        val viewGroup: ViewGroup = binding.tagContent
+        val textView = TextView(requireContext())
+        textView.apply {
+            textSize = 12f
+            setTextColor(Color.BLACK)
+            setBackgroundResource(R.drawable.blue_rounded_corners)
+            text = tag
+            setPadding(30, 5, 30, 5)
 
+        }
+        viewGroup.addView(textView)
     }
 
+    private fun setReadmeInTextView(content: String?) {
+        content?.let {
+            binding.tvReadme.text = String(Base64.decode(it, Base64.DEFAULT))
+        }
+    }
 
+    private fun setupTags(result: List<GetTagsResponseModelItem>) {
+        val subList = mutableListOf<GetTagsResponseModelItem>()
+        if (result.size > 4) {
+            subList.addAll(result.subList(0, 4))
+            subList.forEach {
+                setTagIntoView(it.name ?: "")
+            }
+        } else if (result.isNotEmpty()) {
+            if (result.first().name != null) {
+                result.forEach {
+                    setTagIntoView(it.name ?: "")
+                }
+            }
+        }
+    }
 }
--- TestCoopelRepos/app/src/main/java/com/example/testcoopelrepos/detailRepo/viewModels/DetailRepoFragmentViewModel.kt
+++ TestCoopelRepos/app/src/main/java/com/example/testcoopelrepos/detailRepo/viewModels/DetailRepoFragmentViewModel.kt
@@ -1,8 +1,50 @@
 package com.example.testcoopelrepos.detailRepo.viewModels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.data.generic.LocalResult
+import com.example.data.local.datastore.DataStoreManager
+import com.example.data.local.datastore.PreferencesData
+import com.example.domain.network.response.readme.ReadmeResponseModel
+import com.example.domain.network.response.tags.GetTagsResponseModelItem
+import com.example.usescases.usescases.readme.PerformReadme
+import com.example.usescases.usescases.tags.PerformGetTags
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class DetailRepoFragmentViewModel: ViewModel() {
+class DetailRepoFragmentViewModel @Inject constructor(
+    private val performGetTags: PerformGetTags,
+    private val performReadme: PerformReadme,
+    private val dataStoreManager: DataStoreManager
+) : ViewModel() {
+
+    private val _tagsResult = MutableLiveData<LocalResult<List<GetTagsResponseModelItem>>?>()
+    val tagsResult: LiveData<LocalResult<List<GetTagsResponseModelItem>>?> = _tagsResult
+
+    fun getTagsList(owner: String, repo: String) = viewModelScope.launch(Dispatchers.IO) {
+        val result = performGetTags(owner, repo)
+        _tagsResult.postValue(result)
+    }
+
+
+    private val _readmeResult = MutableLiveData<LocalResult<ReadmeResponseModel>>()
+    val readmeResult: LiveData<LocalResult<ReadmeResponseModel>> = _readmeResult
+
+
+    // html_url
+    fun getReadmeList(owner: String, repo: String) = viewModelScope.launch(Dispatchers.IO) {
+        val result = performReadme(owner, repo)
+        _readmeResult.postValue(result)
+    }
+
+    val userData = MutableLiveData<PreferencesData>()
+
+    suspend fun getUserData() {
+        userData.value = dataStoreManager.getPreferencesData()
+    }
 }
